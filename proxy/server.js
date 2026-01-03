918b262c66be04d58218c25afa46ab0c397bf640
Juroza <jamaine.scarlett@gmail.com>
2026-01-03 21:17:49 +0000

    eiojginagoin

--- proxy/server.js
+++ proxy/server.js
@@ -2,9 +2,12 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import http from "http";
-import { Server } from "socket.io";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 import cors from "cors";
 import axios from "axios";
+const { Server } = require("socket.io");
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
