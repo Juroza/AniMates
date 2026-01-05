c7299f40cbcffe3fe21fb8ce54865e3a8e0758cb
Juroza <jamaine.scarlett@gmail.com>
2026-01-05 16:21:16 +0000

    crossorigin isolated

--- proxy/server.mjs
+++ proxy/server.mjs
@@ -16,7 +16,11 @@ const server = http.createServer(app);
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 app.use(cors({ origin: FRONTEND_URL }));
 app.use(express.json());
-
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
 const BACKEND_ENDPOINT =
   process.env.BACKEND_ENDPOINT || "http://localhost:7071";
 
